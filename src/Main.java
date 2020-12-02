4b805e4883a1d9afc77a42a74a59cfd84f514ee2
Ercan Civi <ercancivi@gmail.com>
2020-12-02 11:53:05 -0600

    fixed one of the print statement's error

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello");
-        System.out.println("Hello Hello);
+        System.out.println("Hello Hello");
         System.out.println("How Are You Today");                   
                            
 
